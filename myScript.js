e68a301c87bd2dab2d0535f96c8c777e5a5ab616
GermanGonzalezCastro <21690109@tecvalles.mx>
2024-10-23 18:37:31 -0600

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+                document.getElementById('demo3').style.fontSize = '40px';
+                document.getElementById("demo3").innerHTML = "Cambió con JS Externo a color VERDE y a 40px";
+                document.getElementById("demo3").style.color = "green";
+            }
+            function aumentar() {
+                document.getElementById('demo2').style.fontSize = '40px';
+                document.getElementById('demo2').style.color = 'green';
+            }
