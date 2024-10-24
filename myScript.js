bdcf3f62029a2925a1228c92adf09369ddcae237
GermanGonzalezCastro <21690109@tecvalles.mx>
2024-10-23 18:47:44 -0600

    Delete myScript.js

--- myScript.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-                document.getElementById('demo3').style.fontSize = '40px';
-                document.getElementById("demo3").innerHTML = "Cambió con JS Externo a color VERDE y a 40px";
-                document.getElementById("demo3").style.color = "green";
-            }
-            function aumentar() {
-                document.getElementById('demo2').style.fontSize = '40px';
-                document.getElementById('demo2').style.color = 'green';
-            }
